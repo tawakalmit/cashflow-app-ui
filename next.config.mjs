a21f3cfb57d97df9e307d9f03bc5a880198a4344
tawakal <mohamad.ikbal@magnusdigital.co.id>
2025-11-15 23:14:31 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  // config lain jika ada
+};
+
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  disable: process.env.NODE_ENV === "development"
+})(nextConfig);
